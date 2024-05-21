49740a2dd237d216bdd9fe8dd0414b9e30ffe688
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-05-21 19:02:18 +0300

    Initial Commit

---
+++ PlaylistSync/PlaylistSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistSyncApp.swift
+//  PlaylistSync
+//
+//  Created by Lasse Wolpmann on 21.5.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
