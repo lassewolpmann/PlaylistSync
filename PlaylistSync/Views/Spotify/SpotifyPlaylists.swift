15210edca3a1d3b092c73f9ac3c9ef1cebe117d1
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-05-22 15:23:23 +0300

    Now loads Spotify Playlists after auth is successful

---
+++ PlaylistSync/Views/Spotify/SpotifyPlaylists.swift
@@ -0,0 +1,36 @@
+//
+//  SpotifyPlaylists.swift
+//  PlaylistSync
+//
+//  Created by Lasse Wolpmann on 22.5.2024.
+//
+
+import SwiftUI
+
+struct SpotifyPlaylists: View {
+    @Environment(SpotifyController.self) private var spotify
+    @State private var playlists: UserPlaylists?
+
+    var body: some View {
+        List {
+            if (playlists != nil) {
+                ForEach(playlists?.items ?? [], id: \.id) { playlist in
+                    Text(playlist.name)
+                }
+            } else {
+                Text("Loading...")
+            }
+        }.task {
+            do {
+                playlists = try await spotify.getUserPlaylists()
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
+#Preview {
+    SpotifyPlaylists()
+        .environment(SpotifyController())
+}
